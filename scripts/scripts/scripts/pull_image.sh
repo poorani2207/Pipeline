a9024a329e3125d850b975144ffab2a405baa748
poorani2207 <105795661+poorani2207@users.noreply.github.com>
2025-12-07 18:16:43 +0530

    Create pull_image.sh

---
+++ scripts/scripts/scripts/pull_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+AWS_REGION="ap-south-1"
+ACCOUNT_ID="<YOUR_AWS_ACCOUNT_ID>"
+REPO_NAME="myapp-repo"
+IMAGE_TAG="latest"
+
+echo "Logging in to Amazon ECR..."
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+echo "Pulling latest image..."
+docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
