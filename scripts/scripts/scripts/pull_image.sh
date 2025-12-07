a60aff9a4229a609926f1181d6435c9743fbb476
poorani2207 <105795661+poorani2207@users.noreply.github.com>
2025-12-07 21:30:22 +0530

    Update pull_image.sh

--- scripts/scripts/scripts/pull_image.sh
+++ scripts/scripts/scripts/pull_image.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
 AWS_REGION="ap-south-1"
-ACCOUNT_ID="<246844015696>"
-REPO_NAME="myapp-repo"
-IMAGE_TAG="latest"
+ACCOUNT_ID="246844015696"
+REPO_NAME="myapp"
 
 echo "Logging in to Amazon ECR..."
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+aws ecr get-login-password --region $AWS_REGION | \
+docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-echo "Pulling latest image..."
+# Read Image tag from CodeBuild imageDetail.json (if exists)
+if [ -f /tmp/deploy/imageDetail.json ]; then
+    IMAGE_TAG=$(jq -r '.imageUri' /tmp/deploy/imageDetail.json | awk -F':' '{print $2}')
+else
+    IMAGE_TAG="latest"
+fi
+
+echo "Pulling image from ECR: $REPO_NAME:$IMAGE_TAG"
+
 docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
