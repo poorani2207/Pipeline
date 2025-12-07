017b55d97a16114470bb836de9dc0ce73742ab84
poorani2207 <105795661+poorani2207@users.noreply.github.com>
2025-12-07 18:17:20 +0530

    Create start_container.sh

---
+++ scripts/scripts/scripts/start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CONTAINER_NAME="myapp"
+PORT=80
+AWS_REGION="ap-south-1"
+ACCOUNT_ID="<YOUR_AWS_ACCOUNT_ID>"
+REPO_NAME="myapp-repo"
+IMAGE_TAG="latest"
+
+echo "Starting new container..."
+docker run -d --name $CONTAINER_NAME -p $PORT:80 $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
