7ce28116350d89ee23bd883fcc494bc631433e6a
poorani2207 <105795661+poorani2207@users.noreply.github.com>
2025-12-08 23:17:16 +0530

    Update pull_image.sh

--- scripts/scripts/scripts/pull_image.sh
+++ scripts/scripts/scripts/pull_image.sh
@@ -8,7 +8,7 @@ echo "Logging in to Amazon ECR..."
 aws ecr get-login-password --region $AWS_REGION | \
 docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-# Read Image tag from CodeBuild imageDetail.json (if exists)
+# Read Image tag from CodeBuild imageDetail.json
 if [ -f /tmp/deploy/imageDetail.json ]; then
     IMAGE_TAG=$(jq -r '.imageUri' /tmp/deploy/imageDetail.json | awk -F':' '{print $2}')
 else
@@ -16,5 +16,4 @@ else
 fi
 
 echo "Pulling image from ECR: $REPO_NAME:$IMAGE_TAG"
-
 docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
