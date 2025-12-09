8a5922c963b4b209612df11718bfe5c31cc39598
poorani2207 <105795661+poorani2207@users.noreply.github.com>
2025-12-09 13:52:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -19,13 +19,15 @@ echo "Stopping old container if exists"
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# 🔥 Kill container using port if exists
+# Kill container using port 8080 if any
 CONTAINER_ON_PORT=$(docker ps -q --filter "publish=$PORT")
 if [ ! -z "$CONTAINER_ON_PORT" ]; then
-    echo "Port $PORT is in use. Stopping container $CONTAINER_ON_PORT..."
-    docker stop $CONTAINER_ON_PORT || true
-    docker rm $CONTAINER_ON_PORT || true
+  echo "Port $PORT is in use. Stopping container $CONTAINER_ON_PORT..."
+  docker stop $CONTAINER_ON_PORT || true
+  docker rm $CONTAINER_ON_PORT || true
 fi
 
 echo "Starting new container..."
 docker run -d --name $CONTAINER_NAME -p $PORT:80 $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
+
+exit 0
